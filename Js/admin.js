1a648a01827cf06ecdd47653765672ac9a14b796
Rhonald S. parra S. <stevendpasua@gmail.com>
2026-08-12 01:12:27 -0500

    se cambia html de lista, por tabla,

--- Js/admin.js
+++ Js/admin.js
@@ -50,12 +50,14 @@ function AgregarProducto(e){
     console.log(inventario);
     console.log(JSON.stringify(inventario,null,2));
     formulario.reset(); // formulario se deja vacio
+    modalProducto.classList.remove("activo");
 
 }
 
 //      renderizamos productos en html
 
 const lista = document.querySelector(".tareas");
+
 function renderizarInventario(){
     lista.innerHTML="";
 
@@ -66,35 +68,45 @@ function renderizarInventario(){
 
         if(producto.stock==0){
             estado="Sin Stock";
-            clase="bg-danger-subtle";
+            clase="bg-danger";
 
         }else if(producto.stock<=5){
             estado="Poco Stock";
-            clase="bg-warning-subtle";
+            clase="bg-warning";
 
         }else{
             estado="Buen Stock";
-            clase="bg-success-subtle";
+            clase="bg-success";
         }
         //      renderizamos cada producto en la lista de tareas -html
         lista.innerHTML +=`
-            <div class="tareaFondo">
-            <p>${producto.nombre}</p>
-            <p>${producto.categoria}</p>
-            <p>${producto.marca}</p>
-            <p>${producto.precio}</p>
-            <p>${producto.stock}</p>
-            <span class="${clase}">
-                ${estado}
-            </span>
-            <i class="bi bi-trash" data-id="${producto.id}"></i>
-            </div>
+            <tr>
+                <td>${producto.id}</td>
+                <td>${producto.nombre}</td>
+                <td>${producto.serie}</td>
+                <td>${producto.categoria}</td>
+                <td>${producto.marca}</td>
+                <td>$ ${producto.precio}</td>
+                <td>${producto.stock}</td>
+                <td>${producto.referencia}</td>
+                <td>
+                    <span class=" badge ${clase}">
+                        ${estado}
+                    </span>
+                </td>
+                <td>
+                    <i class="bi bi-pencil-square text-primary me-3"></i>
+                    <i class="bi bi-trash text-danger"
+                       data-id="${producto.id}"
+                       style="cursor:pointer;"></i>
+                </td>
+            </tr>
         `;
     });
 }
 
 
-//      evento elimina el producto por funcion-id al seleccionar icono
+//      evento busca producto por funcion-id al seleccionar icono
 
 lista.addEventListener("click", (e) => {
     if (e.target.classList.contains("bi-trash")) {
@@ -103,22 +115,38 @@ lista.addEventListener("click", (e) => {
     }
 });
 
-//      funcion
+//      funcion eliminar producto por id
 
 function eliminarProducto(id){
-
     const indice = inventario.findIndex(
         producto => producto.id === id
     );
-
     inventario.splice(indice,1);
     renderizarInventario();
     console.clear();
     console.log(inventario);
 }
 
-modal.classList.add("activo");
-modal.classList.remove("activo");
+// modal para visualizar agregar producto en ventana suspendida
+
+const modalProducto = document.querySelector("#modalProducto");
+const abrirFormulario = document.querySelector("#abrirFormulario");
+const cerrarFormulario = document.querySelector("#cerrarFormulario");
+
+abrirFormulario.addEventListener("click", () => {
+    modalProducto.classList.add("activo");
+});
+
+// cerrar ventana agregar producto en X
+cerrarFormulario.addEventListener("click", () => {
+    modalProducto.classList.remove("activo");
+});
+
+modalProducto.addEventListener("click", (e) => {
+    if(e.target === modalProducto){
+        modalProducto.classList.remove("activo");
+    }
+});
 
 
 
