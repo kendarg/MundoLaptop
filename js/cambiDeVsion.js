5a7d352c2cea500a8b609f93c0ae7aa5dfb14efa
Kendarg Real <estebanreal2010@hotmail.com>
2026-08-14 23:01:47 -0500

    Carrito y reutilizacion de footer

--- js/cambiDeVsion.js
+++ js/cambiDeVsion.js
@@ -1,31 +1,55 @@
-// MODO OSCURO
+// MODO OSCURO Independiente
+//Toco pasarlo a una funcion
+function inicializarModoOscuro() {
+    let modo = document.getElementById("modo");
+    let body = document.body;
+    let icono = document.getElementById("icono");
+    let logo = document.getElementById("logo");
 
-let modo = document.getElementById("modo");
-let body = document.body;
-let icono = document.getElementById("icono");
+    if (!modo) return;
 
-modo.addEventListener("click", function(){
-    let oscuro = body.classList.toggle("dark");
-    localStorage.setItem("modo", oscuro);
+    let v = localStorage.getItem("modo");
 
-    icono.classList.toggle("bi-moon");
-    icono.classList.toggle("bi-sun");
-});
+    if (v === "true") {
+        body.classList.add("dark");
+        if (logo) logo.classList.add("dark-logo");
+        if (icono) {
+            icono.classList.remove("bi-moon");
+            icono.classList.add("bi-sun");
+        }
+    } else {
+        body.classList.remove("dark");
+        body.classList.add("claro");
+        if (logo) logo.classList.remove("dark-logo");
+        if (icono) {
+            icono.classList.add("bi-moon");
+            icono.classList.remove("bi-sun");
+        }
+    }
 
-let v = localStorage.getItem("modo");
+    modo.addEventListener("click", function () {
+        let oscuro = body.classList.toggle("dark");
+        localStorage.setItem("modo", oscuro);
 
-
-if (v === "true") {
-    body.classList.add("dark");
-
-    icono.classList.remove("bi-moon");
-    icono.classList.add("bi-sun");
-
-}else{
-    body.classList.remove("dark");
-    body.classList.add("claro");
-
-    icono.classList.add("bi-moon");
-    icono.classList.remove("bi-sun");
+        if (icono) {
+            icono.classList.toggle("bi-moon");
+            icono.classList.toggle("bi-sun");
+        }
+        if (logo) {
+            logo.classList.toggle("dark-logo");
+        }
+    });
 }
 
+const observer = new MutationObserver((mutations, obs) => {
+    const modo = document.getElementById("modo");
+    if (modo) {
+        inicializarModoOscuro();
+        obs.disconnect();
+    }
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
