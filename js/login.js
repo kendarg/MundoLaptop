a00981328e708b97a8bf8b992c05c04379085976
Kendarg Real <estebanreal2010@hotmail.com>
2026-09-15 14:51:14 -0500

    Login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const API_AUTH = "https://mundolaptopbackend.onrender.com/api/auth/login";
+const API_AUTH = "https://mundolaptopbackend.onrender.com/api/auth";
 const API_USUARIOS = "https://mundolaptopbackend.onrender.com/api/usuarios";
 
 let isLoginMode = true;
